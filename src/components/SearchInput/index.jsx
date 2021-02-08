ae1328e4892b553c67d42b771f0d947a1cb93524
Your Name <you@example.com>
2021-02-08 10:26:29 -0300

    feat: adiconado debounce na buscar dos filmes

--- src/components/SearchInput/index.jsx
+++ src/components/SearchInput/index.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import useStyles from './styles';
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
+import { debounce } from '@material-ui/core';
 
 const SearchInput = ( { onChange } ) => {
     const styles = useStyles();
+
+    const debounceOnChange = debounce( onChange, 300 );
 
     return (
         <div className={ styles.searchBox }>
@@ -13,7 +16,7 @@ const SearchInput = ( { onChange } ) => {
             </div>
             <InputBase
                 placeholder='Buscar filme'
-                onChange={ onChange }
+                onChange={ debounceOnChange }
                 classes={ {
                     root: styles.inputRoot,
                     input: styles.inputInput,
