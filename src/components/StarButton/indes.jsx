952df614f818bd636d212ed905d952bce0aa07ff
Your Name <MBMACEDO@brasilseg.com.br>
2021-02-11 09:36:52 -0300

    feat: estrela de favoritos dourada

--- src/components/StarButton/indes.jsx
+++ src/components/StarButton/indes.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import StarIcon from '@material-ui/icons/Star';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 
-const StarButton = ( { filled } ) => {
-    return filled ? <StarIcon /> : <StarBorderIcon />
-}
+const StarButton = ({ filled }) => {
+    return filled ? <StarIcon /> : <StarBorderIcon />;
+};
 
 export default StarButton;
