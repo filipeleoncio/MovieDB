f227ace3343980c1b7549f61209d596068b912e7
Filipe <filipeleoncio>
2021-02-15 04:24:50 -0300

    reafactor(loading): Ajustes na posição do ícone

--- src/components/Loading/styles.js
+++ src/components/Loading/styles.js
@@ -2,10 +2,8 @@ import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles({
     root: {
-        // display: 'flex',
         position: 'absolute',
-        top: 400,
-        // top: '50%',
+        top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
     },
